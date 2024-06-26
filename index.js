16eee0e36fff76734c194bd39f0dfa4ee6d056dc
Mase1up <mason.l.keith@gmail.com>
2024-06-26 18:16:29 -0400

    Reattack code on own

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-let count = 0;
-let countEl = document.getElementById("count-el")
+// let count = 0;
+// let countEl = document.getElementById("count-el");
 
+
+// function increment() {
+//     count = count + 1;
+//     countEl.innerText = count;
+//     console.log(count)
+// }
+
+let count = 0;
+let countEl = document.getElementById("count-el");
 
 function increment() {
     count = count + 1;
     countEl.innerText = count;
-    console.log(count)
+    console.log(count);
 }
-
