c78e175ddbb07329c36c679437f5aa79b35a9ecb
Mase1up <mason.l.keith@gmail.com>
2024-06-26 16:12:27 -0400

    Add onclick event to button

--- index.js
+++ index.js
@@ -4,12 +4,9 @@
 // count = count + 1;
 // console.log(count);
 
-// Create var bonusPoints initialized as 50.  Increase it to 100,
-// Decrease it down to 25, finally increase it to 70
-
-let bonusPoints = 50;
-bonusPoints = bonusPoints + 50;
-bonusPoints = bonusPoints - 75;
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+// Psuedo-code to create incrementing button
+// Initialize count @ 0
+// Listen for clicks on increment button
+// When clicked, increment the count variable
+// change the count-el to reflect new count
 
