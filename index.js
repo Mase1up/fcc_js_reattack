e58fee998c8bba2142747aa940ea757a0cc5455f
Mase1up <mason.l.keith@gmail.com>
2024-06-26 15:31:20 -0400

    First challenge done

---
+++ index.js
@@ -0,0 +1,17 @@
+//document.getElementById("count-el").innerText=5
+
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+
+
+// Create two variables, myAge and humanDogRatio
+// Multiply them together and store result in myDogAge variable
+// Log myDogAge to console
+
+let myAge = 41;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
