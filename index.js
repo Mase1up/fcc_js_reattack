f2ae18a3b7fc72ea0311b1a9aa578811abea6869
Mase1up <mason.l.keith@gmail.com>
2024-06-26 17:29:55 -0400

    Clean up JS code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 let count = 0;
 let countEl = document.getElementById("count-el")
 
-console.log(countEl);
-
-// Psuedo-code to create incrementing button
-
-// change the count-el to reflect new count
 
 function increment() {
     count = count + 1;
