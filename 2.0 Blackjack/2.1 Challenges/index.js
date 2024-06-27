99472e057360a9a9a21ee3ff27855a563d5aff61
Mase1up <mason.l.keith@gmail.com>
2024-06-27 18:05:06 -0400

    Add if logic to blackjack, chal#1

---
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -0,0 +1,10 @@
+// Check if person is old enough to enter the night club (21)
+// Log suitable message to console for either case
+
+let age = 41
+
+if (age >= 21) {
+    console.log("Come on in!");
+} else {
+    console.log("You must be 21 to enter");
+}
--- 2.0 Blackjack/index.js
+++ 2.0 Blackjack/index.js
@@ -3,9 +3,17 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 7;
+let firstCard = 9;
 let secondCard = 4;
 
 let sum = firstCard + secondCard;
 
 console.log(sum);
+
+if (sum < 21) {
+    console.log("Do you want to hit?");
+} else if (sum === 21) {
+    console.log("Blackjack!  Congratulations!");
+} else {
+    console.log("Sorry, you busted.  Play again?");
+}
