1b123b0bd16fe062516d349fd5ace786c50247d1
Mase1up <mason.l.keith@gmail.com>
2024-06-28 16:03:50 -0400

    Function to roll a die

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -1,7 +1,15 @@
 
 // Simulate a Die Roll
 
-let randomNumber = (Math.floor(Math.random() * 6)) + 1;
+// let randomNumber = (Math.floor(Math.random() * 6)) + 1;
 
-console.log(randomNumber);
+// console.log(randomNumber);
 
+// Create function to return 1-6 at random
+
+function rollDice() {
+    return (Math.floor(Math.random() * 6)) + 1;
+}
+
+let randomResult = rollDice();
+console.log(randomResult);
