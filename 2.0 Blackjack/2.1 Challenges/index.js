346303ee77b138961f8d585060bde7bf8ecf417a
Mase1up <mason.l.keith@gmail.com>
2024-06-28 15:25:56 -0400

    Practice using a return function to store data in a var

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -1,19 +1,30 @@
 let player1Time = 102;
 let player2Time = 107;
 
-function getFastestRaceTime() {
-    if (player1Time < player2Time) {
-        return player1Time;
-    } else if (player2Time < player1Time) {
-        return player2Time;
-    } else {
-        return "It was a tie";
-    }
+// function getFastestRaceTime() {
+//     if (player1Time < player2Time) {
+//         return player1Time;
+//     } else if (player2Time < player1Time) {
+//         return player2Time;
+//     } else {
+//         return "It was a tie at " + player1Time;
+//     }
+// }
+
+// // This worked, but not the way the function would be used
+// // console.log(getFastestRaceTime());
+
+// let fastestRaceTime = getFastestRaceTime();
+
+// console.log(fastestRaceTime);
+
+// Write function that returns total race time
+// Invoke func and store return in new var
+// Log the var out
+
+function totalRaceTime() {
+    return player1Time + player2Time;
 }
 
-// This worked, but not the way the function would be used
-// console.log(getFastestRaceTime());
-
-let fastestRaceTime = getFastestRaceTime();
-
-console.log(fastestRaceTime);
+let totalTime = totalRaceTime();
+console.log(totalTime);
