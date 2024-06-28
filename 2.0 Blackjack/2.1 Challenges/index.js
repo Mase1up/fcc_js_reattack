232040bc89204c3909c636361450cf3e3fae2b9a
Mase1up <mason.l.keith@gmail.com>
2024-06-28 12:26:01 -0400

    Use returns to assign value to variable with a function

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -1,13 +1,19 @@
-let sentence = [
-    "Hello,",
-    "my",
-    "name",
-    "is",
-    "Mason"
-];
+let player1Time = 102;
+let player2Time = 107;
 
-let greetingEl = document.getElementById("greeting-el");
-
-for (i = 0; i < sentence.length; i++) {
-    greetingEl.textContent += sentence[i] + " ";
+function getFastestRaceTime() {
+    if (player1Time < player2Time) {
+        return player1Time;
+    } else if (player2Time < player1Time) {
+        return player2Time;
+    } else {
+        return "It was a tie";
+    }
 }
+
+// This worked, but not the way the function would be used
+// console.log(getFastestRaceTime());
+
+let fastestRaceTime = getFastestRaceTime();
+
+console.log(fastestRaceTime);
