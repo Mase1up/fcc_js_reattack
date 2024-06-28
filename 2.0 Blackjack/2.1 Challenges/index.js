15095d406ad866430345cfcdcbecd42e311cce19
Mase1up <mason.l.keith@gmail.com>
2024-06-28 15:52:46 -0400

    Make the random integer be 1-6

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -1,22 +1,7 @@
 
 // Simulate a Die Roll
 
-// let randomNumber = Math.random() * 6;
+let randomNumber = (Math.floor(Math.random() * 6)) + 1;
 
-// console.log(randomNumber);
+console.log(randomNumber);
 
-/*
-What does Math.random() actually do?
-
-Your answer:
-Should return a random number between 0 and 5.999 repeating
-
-
-*/
-
-let flooredNumber = Math.floor(3.45632);
-console.log(flooredNumber);
-
-// It truncates everthing to the right of the decimal.  
-// This isn't quite accurate, it actually moves to the leftmost possible integer
-// on the number line.  If it were -2.36 it would become -3
