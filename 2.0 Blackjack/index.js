8fb7595604747c1fc1fe4faf00679f4d3a42b0e1
Mase1up <mason.l.keith@gmail.com>
2024-06-28 08:56:14 -0400

    Add functional startGame btn with CSS

--- 2.0 Blackjack/index.js
+++ 2.0 Blackjack/index.js
@@ -1,7 +1,3 @@
-// 1. Create two variables, firstCard and secondCard
-// Set their values to random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard = 10;
 let secondCard = 11;
@@ -12,17 +8,19 @@ let message = "";
 
 // console.log(sum);
 
-if (sum < 21) {
-    message = "Do you want to hit?";
-} else if (sum === 21) {
-    hasBlackjack = true;
-    message = "Blackjack!  Congratulations!";
-} else {
-    isAlive = false;
-    message = "Sorry, you busted.  Play again?";
-}
+function startGame() {
+    isAlive = true;
+    hasBlackjack = false;
 
-// CASH OUT
-// console.log(hasBlackjack);
-// console.log(isAlive);
-console.log(message);
+    if (sum < 21) {
+        message = "Do you want to hit?";
+    } else if (sum === 21) {
+        hasBlackjack = true;
+        message = "Blackjack!  Congratulations!";
+    } else {
+        isAlive = false;
+        message = "Sorry, you busted.  Play again?";
+    }
+
+    // console.log(message);
+}
