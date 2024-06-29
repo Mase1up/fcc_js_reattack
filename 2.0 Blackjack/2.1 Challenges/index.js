13bab8f0242c90f3599fa4d4b8f8328bfc2cd464
Mase1up <mason.l.keith@gmail.com>
2024-06-29 01:54:52 -0400

    Finish the fruit sort challenge

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -33,7 +33,31 @@ function altGame() {
     return hands[randomNumber];
 }
 
-console.log(altGame());
+// console.log(altGame());
 
 // The above is exactly how Per did it, and I was trying so hard to
 // figure out how to do it without bracket notation.
+
+
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"];
+let appleShelf = document.getElementById("apple-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
+
+// Create a function that puts the apples on the appleshelf
+// and the oranges on the orangeshelf.  Use a for loop,
+// a conditional statement, and the textContent property.
+
+
+function fruitSort() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎";
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊";
+        } else {
+            console.log("Error, something other than an apple or orange is in the array");
+        }
+    }
+}
+
