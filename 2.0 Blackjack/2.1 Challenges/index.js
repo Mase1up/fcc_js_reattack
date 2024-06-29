bca43f1bf9f6a9452ed31baefa62452dd6680883
Mase1up <mason.l.keith@gmail.com>
2024-06-28 23:00:33 -0400

    Practice for loops

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -1,26 +1,10 @@
-// Conditional statement to log out the discount the passenger
-// will get based on age
+// Use a for loop to log out an array
 
-// less than 6 years old -> free
-// 6-17 -> child discount
-// 18-26 -> student discount
-// 27-66 -> full price
-// over 66 -> senior citizen discount
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
 
-let age = 69;
 
-function checkDiscount() {
-    if (age < 6) {
-        return "free";
-    } else if (age < 18) {
-        return "child discount";
-    } else if (age < 27) {
-        return "student discount";
-    } else if (age < 67) {
-        return "full price";
-    } else {
-        return "senior citizen discount";
-    }
+console.log("The 5 largest countries in the world:")
+
+for (i = 0; i < largeCountries.length; i++) {
+    console.log("- " + largeCountries[i]);
 }
-
-console.log(checkDiscount());
