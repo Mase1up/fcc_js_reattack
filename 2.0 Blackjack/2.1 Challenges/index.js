4c303a662e4f302956df03928705ba18346ab2e6
Mase1up <mason.l.keith@gmail.com>
2024-06-29 01:30:20 -0400

    Practice with returning random item from an array

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -4,4 +4,36 @@ largeCountries.pop();
 largeCountries.push("Pakistan");
 largeCountries.shift();
 largeCountries.unshift("China");
-console.log(largeCountries);
+// console.log(largeCountries);
+
+let dayOfMonth = 13;
+let dayOfWeek = "Friday";
+
+if (dayOfMonth === 13 && dayOfWeek === "Friday") {
+    // console.log("Spooky Emoji");
+}
+
+// Create a function that returns a random item from the following array
+
+let hands = ["rock", "paper", "scissors"]
+
+function playGame() {
+    //Generate random number 0-2
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(hands[randomNumber]);
+}
+
+// playGame();
+
+
+
+
+function altGame() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    return hands[randomNumber];
+}
+
+console.log(altGame());
+
+// The above is exactly how Per did it, and I was trying so hard to
+// figure out how to do it without bracket notation.
