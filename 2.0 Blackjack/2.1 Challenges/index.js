0ec32936624be7aa4c5fbf41e4102162e1a35ede
Mase1up <mason.l.keith@gmail.com>
2024-06-28 22:43:41 -0400

    Use object and a function to create a string

--- 2.0 Blackjack/2.1 Challenges/index.js
+++ 2.0 Blackjack/2.1 Challenges/index.js
@@ -1,11 +1,19 @@
-// Create object for airBnB listings
+// Create a person object containing 3 keys: name, age, and country
 
-let listing = {
-    superhost: true,
-    hostName: "Per",
-    price: 190,
-    tags: ["castle", "fan-favorite"]
+let person = {
+    name: "Mason",
+    age: 41,
+    country: "USA"
 }
 
-console.log(listing.price);
-console.log(listing.tags);
+// Create a function logData() that uses the person object to create a
+// string in the following format:
+// "Mason is 41 years old and lives in USA"
+
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country + ".")
+}
+
+//Call the logData() function to verify it works
+
+logData();
