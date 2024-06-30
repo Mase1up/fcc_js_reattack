511077368535e7239b2eee2028b60d38d0125a60
Mase1up <mason.l.keith@gmail.com>
2024-06-29 22:04:40 -0400

    Start using const

--- 3.0 Chrome Extension/index.js
+++ 3.0 Chrome Extension/index.js
@@ -1,7 +1,12 @@
 // Log out "Button clicked!" when user clicks SAVE btn
 
-let saveBtn = document.getElementById("save-btn");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", function () {
-    console.log("Button clicked from addEventListener");
+    console.log("Button clicked!");
 });
+
+
+
