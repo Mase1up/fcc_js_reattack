73c9e42aabbb3b10ca447aea4d995c254d5f8a89
Mase1up <mason.l.keith@gmail.com>
2024-06-29 22:41:22 -0400

    Successfully adding new HTML elements into the div via JS

--- 3.0 Chrome Extension/3.1 Challenges/index.js
+++ 3.0 Chrome Extension/3.1 Challenges/index.js
@@ -4,7 +4,17 @@
 
 const boxEl = document.getElementById("box-el");
 
-boxEl.addEventListener("click", function () {
-    console.log("I want to open the box!");
-});
+// boxEl.addEventListener("click", function () {
+//     console.log("I want to open the box!");
+// });
 
+// Use .innerHTML to render a Buy! button inside the div.
+
+// When clicked, render a para under the button (in the container), 
+// that says "Thank you for buying!"
+
+boxEl.innerHTML = " <button id='buy-btn' onclick='buy()'>Buy!</button> ";
+
+function buy() {
+    boxEl.innerHTML += "<p>Thank you for buying!</p>"
+}
